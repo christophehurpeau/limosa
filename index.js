6c69af110ff33ed8404f9ee11ac12df9b1b6514b
Christophe Hurpeau <christophe@evaneos.com>
2014-05-02 10:02:31 +0200

    1.0.2 fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
-    RoutesTranslations: require('lib/routes_translations'),
-    RouterBuilder: require('lib/builder')
+    RoutesTranslations: require('./lib/routes_translations'),
+    RouterBuilder: require('./lib/builder')
 };
